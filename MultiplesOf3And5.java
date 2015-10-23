a8aae31e697dffa4ba7bc8199293d478df16cb29
Viveka <aggarwalviveka@gmail.com>
2015-10-23 17:31:18 -0400

    Update MultiplesOf3And5.java

--- MultiplesOf3And5.java
+++ MultiplesOf3And5.java
@@ -1,18 +1,12 @@
 // program to compute the sum of all multiples of 3, 5 below 100.
 
-import java.util.HashMap;
-
 public class MultiplesOf3And5 {
 	public static void main(String[] args) {
-		HashMap<Integer,Boolean> map = new HashMap<>();	
 		long sum = 0;
 		for(int i = 3; i < 100 ; i++) {
 			if(i % 3 == 0 || i % 5 == 0) {
-				if(!map.containsKey(i)) {
-					map.put(i, true);
 					System.out.print(i + " ");
 					sum+=i;
-				}
 			}
 		}
 		System.out.print("\nThe sum of is: "+sum);
